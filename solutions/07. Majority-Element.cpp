97bc9bdc99904f850e31d178854393ae2b6e051f
--replace-all <anshulgupta4905@gmail.com>
2023-11-19 07:43:10 +0530

    Day-4

---
+++ solutions/07. Majority-Element.cpp
@@ -0,0 +1,20 @@
+// ******** Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array ********
+
+// <======== Example ========>
+// Input: nums = [3,2,3]
+// Output: 3
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int mid = nums.size() / 2;
+        return nums[mid];
+    }
+};
