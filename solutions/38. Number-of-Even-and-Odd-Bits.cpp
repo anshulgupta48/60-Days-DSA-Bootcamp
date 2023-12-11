9f393dc0ec23aebef8e4b0b873c749d8a219d341
Anshul Gupta <124188230+anshulgupta48@users.noreply.github.com>
2023-12-11 21:17:37 +0530

    Day-19

--- solutions/38. Number-of-Even-and-Odd-Bits.cpp
+++ solutions/38. Number-of-Even-and-Odd-Bits.cpp
@@ -1,4 +1,4 @@
-// ******** You are given a positive integer n. Let even denote the number of even indices in the binary representation of n (0-indexed) with value 1. Let odd denote the number of odd indices in the binary representation of n (0-indexed) with value 1. Return an integer array answer where answer = [even, odd]. ********
+// ******** You are given a positive integer n, Let even denote the number of even indices in the binary representation of n (0-indexed) with value 1. Let odd denote the number of odd indices in the binary representation of n (0-indexed) with value 1. Return an integer array answer where answer = [even, odd]. ********
 
 // <======== Example ========>
 // Input: n = 17
